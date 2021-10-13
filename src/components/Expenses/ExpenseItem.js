82effd85fefe2dd8ac67c38e5ad232aa636560e2
Gulka_D <d.gulka4@gmail.com>
2021-10-13 18:41:23 +0200

    Fisrt useState hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+    
+    const [title, setTitle] = useState(props.title);
+
+    const updateTitle = () => {
+        setTitle('NewTitle!');
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">$ {props.amount}</div>
             </div>
+            <button onClick={updateTitle}>Change title</button>
         </Card>
     );
 };
