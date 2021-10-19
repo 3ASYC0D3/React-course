889323aa9256882fd2a10ffcfd2aa47697fbb034
Gulka_D <d.gulka4@gmail.com>
2021-10-19 15:29:54 +0200

    Move expenses list to own component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 
 
 
 const Expenses = (props) => {
-
     const [filterYear, setFilterYear] = useState('2020');
 
     const filterChangeHandler = (selectedYear) => {
@@ -16,23 +15,15 @@ const Expenses = (props) => {
         console.log(selectedYear);
     };
     
-    
     const filteredExpenses = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === filterYear;
     });
-    
 
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filterYear} onChangeFilter={filterChangeHandler}/>
-                {filteredExpenses.map((expense) => (
-                    <ExpenseItem
-                    key={expense.id}
-                    title={expense.title} 
-                    amount={expense.amount} 
-                    date={expense.date} />
-                ))};
+                <ExpensesList items={filteredExpenses} />
             </Card>
         </div>
     )
